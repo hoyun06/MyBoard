df9304f797157e68dec951f377e1f524e0331053
hoyun06 <hoyun06@gmail.com>
2022-08-29 23:40:33 +0900

    게시물에 링크 설정, 게시물 수정 기능 추가, 입력 validation 기능 추가

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -2,12 +2,14 @@ package com.example.board.controller;
 
 import com.example.board.model.Board;
 import com.example.board.repository.BoardRepository;
+import com.example.board.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -17,6 +19,9 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
 
@@ -24,6 +29,24 @@ public class BoardController {
         model.addAttribute("board", board);
         return "board/list";
     }
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false, name = "id") Long id) {
+        if(id == null)
+            model.addAttribute("board", new Board());
+        else
+            model.addAttribute("board", boardRepository.findById(id).orElse(null));
+        return "board/form";
+    }
 
+    @PostMapping("/form")
+    public String write(@Valid Board board, BindingResult bindingResult) {
 
+        boardValidator.validate(board,bindingResult);
+
+        if(bindingResult.hasErrors())
+            return "board/form";
+
+        boardRepository.save(board);
+        return "redirect:/board/list";
+    }
 }
