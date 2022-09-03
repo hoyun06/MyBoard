7a1e9c6604537860221179e4eb685fe4dc4d9a22
hoyun06 <hoyun06@gmail.com>
2022-09-03 23:45:45 +0900

    jpa 이용하여 RESTful API 작성, BoardApiController 작성

--- src/main/java/com/example/board/repository/BoardRepository.java
+++ src/main/java/com/example/board/repository/BoardRepository.java
@@ -4,6 +4,11 @@ import com.example.board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findByTitle(String title);
+    List<Board> findByTitleOrContent(String title, String content);
 }
